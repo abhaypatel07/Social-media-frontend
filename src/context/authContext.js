74f8b504569191ce36c865edfcfa3e95eee6ef8a
abhaypatel07 <abhaypatel1607@gmail.com>
2023-12-27 20:43:11 +0530

    authentication

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -3,15 +3,18 @@ import React, { createContext, useEffect, useState } from "react";
 export const authContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user") || null)
-  );
+  const [currentUser, setCurrentUser] = useState({
+    id: 1,
+    name: "Abhay Patel",
+    profile:
+      "https://images.pexels.com/photos/17822820/pexels-photo-17822820/free-photo-of-half-naked-brunette-relaxing-on-a-beach-blanket.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  });
 
   const login = () => {
     //to do
     setCurrentUser({
       id: 1,
-      name: "John Doe",
+      name: "Abhay Patel",
       profile:
         "https://images.pexels.com/photos/17822820/pexels-photo-17822820/free-photo-of-half-naked-brunette-relaxing-on-a-beach-blanket.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     });
